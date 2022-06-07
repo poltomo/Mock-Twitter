15224acd30396927380dbcc5c4817a078e1c2ce6
Varun Nawathey <vnaw4988@gmail.com>
2022-06-07 03:40:33 -0700

    added user service + user controller operations + mappings

--- src/main/java/com/poltomo/HibernateDemo/controllers/UserController.java
+++ src/main/java/com/poltomo/HibernateDemo/controllers/UserController.java
@@ -1,8 +1,27 @@
 package com.poltomo.HibernateDemo.controllers;
 
+import com.poltomo.HibernateDemo.models.User;
+import com.poltomo.HibernateDemo.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class UserController {
+    @Autowired
+    private UserService userService;
 
+    @GetMapping("/users")
+    public List<User> getAllUsers() {
+        return userService.findAll();
+    }
+
+    @GetMapping("/users/{id}")
+    public Optional<User> getUserById(@PathVariable Integer id) {
+        return userService.findById(id);
+    }
 }
--- src/main/java/com/poltomo/HibernateDemo/services/UserService.java
+++ src/main/java/com/poltomo/HibernateDemo/services/UserService.java
@@ -1,8 +1,24 @@
 package com.poltomo.HibernateDemo.services;
 
+import com.poltomo.HibernateDemo.models.User;
+import com.poltomo.HibernateDemo.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
 
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> findAll() {
+        return (List<User>) userRepository.findAll();
+    }
+
+    public Optional<User> findById(Integer id) {
+        return userRepository.findById(id);
+    }
 }
